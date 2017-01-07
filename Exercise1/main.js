324d3d1a1fc2ba8c284d3c1561e8ca6a8a3408d8
CLRB17 <cynthia@brown-house.net>
2017-01-07 01:34:27 -0500

    Cynthia Exercise 1 Answers

---
+++ Exercise1/main.js
@@ -0,0 +1,16 @@
+/*=========================================================
+    Part 1: Variables
+*/
+var givenName;
+// Q: What value is stored in `givenName` right now?
+// A:There is no value
+
+
+givenName = "Brett";
+// Q: What is `givenName` set to now?
+// A:Brett
+
+
+givenName = givenName;
+// Q: What is `givenName` set to now?
+// A:givenName
