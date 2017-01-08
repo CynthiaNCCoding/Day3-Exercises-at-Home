e1c013a23a7a3e9bc6d4454655a0ebd394b0fa5c
CLRB17 <cynthia@brown-house.net>
2017-01-08 14:17:23 -0500

    Exercise3 4 math operators

---
+++ Exercise3/main.js
@@ -0,0 +1,24 @@
+/*=========================================================
+     Part 2: Simple Math
+*/
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// A: 40
+
+math = high - "5";
+// Q: What is `math` set to?
+// A: undefined
+
+
+math = high + low
+A:: 60
+
+math = high * low
+A:500
+
+math = high / low
+A: 5
