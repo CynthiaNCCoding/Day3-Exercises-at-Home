f54ef0b5fd90743d3980539f5f997752c0eaae68
CLRB17 <cynthia@brown-house.net>
2017-01-08 19:19:32 -0500

    Exercise5 Boolean

---
+++ Exercise5/Exercise5.js
@@ -0,0 +1,15 @@
+Take the following two variables:
+
+var myAge = 45;
+//Write a conditional statement that will test if an age is over 100. If it is
+//set a variable isOld to true otherwise set it to false.
+
+var isOld;
+
+if (myAge > 35) {
+  isOld = true;
+} else {
+  isOld = false;
+}
+console.log(isOld);
+}
